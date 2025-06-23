8f8d5417f3473e76ea87dbbb85794508862fb4c9
TilniKaveesha <103948514+TilniKaveesha@users.noreply.github.com>
2025-06-23 14:44:09 +0530

    final

--- app/[locale]/checkout/page.tsx
+++ app/[locale]/checkout/page.tsx
@@ -12,5 +12,5 @@ export default async function CheckoutPage() {
   if (!session?.user) {
     redirect('/sign-in?callbackUrl=/checkout')
   }
-  return <CheckoutForm />
+  return <CheckoutForm userEmail={session.user.email||''} />
 }
